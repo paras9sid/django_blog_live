0c67675d3461dcc12968248935894a9fb4bc1d3e
Sidharth Jain <sidharthjainp9@gmail.com>
2026-04-06 20:32:53 +0530

    eb set

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -31,15 +31,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 # CUSTOM 404 ERROR PAGE - will modify css static files and css will not load- When Deploy-Production server only
 
-# DEBUG = False
+DEBUG = False
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # CSRF_TRUSTED_ORIGINS = []
 
