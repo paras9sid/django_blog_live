b88c89fda02b1b6094f6754fae6f436a0de47ed6
Sidharth Jain <sidharthjainp9@gmail.com>
2026-04-18 16:49:13 +0530

    Railway-app-deployed with genrateed domain- data to populate and transfer from aws to railway full pending

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -31,17 +31,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 # CUSTOM 404 ERROR PAGE - will modify css static files and css will not load- When Deploy-Production server only
 
-DEBUG = False
+# DEBUG = False
 
-ALLOWED_HOSTS = ['djangobloglive-production.up.railway.app', '*']
+# ALLOWED_HOSTS = ['djangobloglive-production.up.railway.app', '*']
 
-CSRF_TRUSTED_ORIGINS = ['https://djangobloglive-production.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://djangobloglive-production.up.railway.app']
 
 # Application definition
 
