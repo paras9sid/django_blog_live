c6f0139db46053b6f2fcf3d5d7f958b7ef0ae524
Sidharth Jain <sidharthjainp9@gmail.com>
2026-04-22 13:25:11 +0530

    v1-updated

--- blog_main/views.py
+++ blog_main/views.py
@@ -41,7 +41,7 @@ def register(request):
         if form.is_valid():
             current_user = form.save(commit=False)
             form.save()
-            send_mail("Welcome to Django Blogs","Congratulations on creating an account on Django Blogs app by Sidharth Jain!!",settings.DEFAULT_FROM_EMAIL,[current_user.email])
+            send_mail("Welcome to Django Blogs","Congratulations on creating an account on Django Blogs app - Sidharth Jain.",settings.DEFAULT_FROM_EMAIL,[current_user.email])
             return redirect('login')
         
     else:
