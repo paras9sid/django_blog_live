4c6b4d9156f843473074b636440b2bf7e7d1899e
Sidharth Jain <sidharthjainp9@gmail.com>
2026-04-23 14:17:02 +0530

    settings-updated-for-s3-railway

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -31,17 +31,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 # CUSTOM 404 ERROR PAGE - will modify css static files and css will not load- When Deploy-Production server only
 
-# DEBUG = env('DEBUG')
+DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['djangoblog.in','djangobloglive-production.up.railway.app', '*']
+ALLOWED_HOSTS = ['djangoblog.in','djangobloglive-production.up.railway.app', '*']
 
-# CSRF_TRUSTED_ORIGINS = ['https://www.djangoblog.in','https://djangobloglive-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://www.djangoblog.in','https://djangobloglive-production.up.railway.app']
 
 # Application definition
 
